ffcb1f9ffd9d0f27831d9fc59aa72243f25328a9
weiwei <weiwei@weiweideMacBook-Air.local>
2022-08-25 00:11:38 +0800

    style modification

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import { useEffect, useState } from 'react';
-import styled from "styled-components";
 import { useImmer } from 'use-immer';
 import './App.css';
-
-const Title = styled.div`
-width:200px;
-height:200px;
-color:black;
-`;
+import { Title, Board, Counter, Column, SubTitle, SubTitleWrapper,
+  NextButton, Waiting, BelowPart } from './components/Counter';
 
 function App() {
   let initialState = {
@@ -16,102 +11,80 @@ function App() {
     {name: "Bob", status : "idle", customerNum: 0, proceeded: []},
     {name: "Cory", status : "idle", customerNum: 0, proceeded: []},
     {name: "Dora", status : "idle", customerNum: 0, proceeded: []},
-    ]
+    ],
+    waitingLine:[]
   }
   const [currentNum, setCurrentNum] = useState(1)
   const [countInfo, setCountInfo] = useImmer(initialState)
-  console.log(countInfo)
-  const [waitingLine, setWaitingLine] = useImmer([])
-  console.log("waiting的人：", waitingLine, "waiting長度", waitingLine.length)
-
+  console.log(countInfo);
   function getNumCard(){
     setCurrentNum((prev)=>prev +1)
-    setWaitingLine((draft)=> {draft.push(currentNum)})
+    setCountInfo((draft)=> {draft.waitingLine.push(currentNum)})
   }
 
   useEffect(()=>{
     function checkCounter(){
       console.log("拿到號碼牌了")
       for(let i = 0; i< countInfo?.counters?.length; i++){
-        if(countInfo.counters[i].status === "idle" && waitingLine.length !== 0){
+        if(countInfo.counters[i].status === "idle" && countInfo.waitingLine.length !== 0){
           console.log("跑進for loop的if了")
-          setCountInfo((draft)=> {draft.counters[i].customerNum = waitingLine[0]})
+          setCountInfo((draft)=> {draft.counters[i].customerNum = draft.waitingLine[0]})
           setCountInfo((draft)=> { draft.counters[i].status = "busy"})
-          setWaitingLine((draft)=>{draft.shift()})
+          setCountInfo((draft)=>{draft.waitingLine.shift()})
           break;
-        }
-      }
-      for(let i =0; i<countInfo?.counters?.length; i++){
-        if(countInfo.counters[i].status === "busy"){
-          setTimeout(()=> {
-            setCountInfo((draft)=>{
-            draft.counters[i].status = "idle"})
-            setCountInfo((draft)=>{
-              draft.counters[i].proceeded.push(draft?.counters[i]?.customerNum)})
-            setCountInfo((draft)=>{
-              draft.counters[i].customerNum = 0})
-          }, getRandomTime(0.5, 1.5))
         }
       }
     }
     checkCounter()
-  },[setCountInfo, setWaitingLine, waitingLine, countInfo.counters])
+  },[countInfo.counters, countInfo.waitingLine.length, setCountInfo])
 
 
   function getRandomTime(min, max) {
     console.log(Math.random() * (max - min) + min);
     return((Math.random() * (max - min) + min)*1000)
   }
-  
-  // useEffect(()=>{
-  //   function processing(){
-  //     for(let i =0; i<countInfo?.counters?.length; i++){
-  //       if(countInfo.counters[i].status === "busy"){
-  //         setTimeout(()=> {
-  //           setCountInfo((draft)=>{
-  //           draft.counters[i].status = "idle"})
-  //           setCountInfo((draft)=>{
-  //             draft.counters[i].proceeded.push(draft?.counters[i]?.customerNum)})
-  //           setCountInfo((draft)=>{
-  //             draft.counters[i].customerNum = 0})
-  //         }, getRandomTime(0.5, 1.5))
-  //       }
-  //     }
-  //   }
-  //   processing()
-  // },[setCountInfo, countInfo.counters])
-  
-  // function processing(){
-  //   for(let i =0; i<countInfo?.counters?.length; i++){
-  //     if(countInfo.counters[i].status === "busy"){
-  //       setTimeout(()=> {
-  //         setCountInfo((draft)=>{
-  //         draft.counters[i].status = "idle"})
-  //         setCountInfo((draft)=>{
-  //           draft.counters[i].proceeded.push(draft?.counters[i]?.customerNum)})
-  //         setCountInfo((draft)=>{
-  //           draft.counters[i].customerNum = 0})
-  //       }, getRandomTime(0.5, 1.5))
-  //     }
-  //   }
-  // }
+
+  useEffect(()=>{
+  function processing(i){
+    for(let i =0; i<countInfo?.counters?.length; i++){
+      if(countInfo.counters[i].status === "busy" && countInfo.counters[i].customerNum !== 0){
+        setTimeout(()=> {
+          setCountInfo((draft)=>{
+          draft.counters[i].status = "idle"})
+          console.log("if", countInfo.counters[i].customerNum)
+          setCountInfo((draft)=>{
+            draft.counters[i].proceeded.push(draft?.counters[i]?.customerNum)})
+          setCountInfo((draft)=>{
+            draft.counters[i].customerNum = 0})
+        }, getRandomTime(0.5, 1.5))
+      }
+    }
+  }
+  processing()
+},[countInfo.counters, setCountInfo])
 
   return (
     <div className="App">
       <Title>Bank Counter</Title>
+      <Board>
+        <SubTitleWrapper>
+          <SubTitle>counter</SubTitle>
+          <SubTitle>processing</SubTitle>
+          <SubTitle>proceeded</SubTitle>
+        </SubTitleWrapper>
       {countInfo?.counters?.map((item, index)=>(
-        <div key={item.name} style={{display: 'flex', flexDirection: "column"}}>
-          <div>工作人員名稱{item.name}</div>
-          <div>此櫃檯狀態 {item.status} </div>
-          <div>幾號在辦理 {item.customerNum}</div>
-        </div>
+        <Counter key={item?.name}>
+          <Column>{item.name}</Column>
+          {/* <Column>此櫃檯狀態 {item.status} </Column> */}
+          <Column>{item.status === "idle" ? item.status : item.customerNum}</Column>
+          <Column>{item.proceeded?.join(", ")}</Column>
+        </Counter>
       ))}
-      <button style={{ height:"30px" }} onClick={()=> { getNumCard()}}>Next {currentNum}</button>
-      {/* <button onClick={()=> checkCounter()}>test</button> */}
-      <div style={{display: "flex", flexDirection: "column"}}>
-        <div>多少人在等 {waitingLine.length}</div>
-        <div>等待的號碼 {waitingLine?.map((item)=> (<div key={item}>{item}</div>))}</div>
-      </div>
+      <BelowPart>
+        <Waiting>Waiting：{countInfo.waitingLine.length}</Waiting>
+        <NextButton onClick={()=> getNumCard()}>Next {currentNum}</NextButton>
+      </BelowPart>
+      </Board>
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+
+export const Title = styled.div`
+font-size:30px;
+font-weight:600;
+color:black;
+`;
+
+export const Board = styled.div`
+width:60%;
+height:auto;
+background-color: beige;
+border-radius: 10px;
+margin-top: 20px;
+display: flex;
+flex-direction:column;
+align-items: center;
+gap:10px;
+`
+
+export const Counter = styled.div`
+width:100%;
+height:60px;
+border:1px black solid;
+border-radius: 10px;
+display:flex;
+justify-content: center;
+align-items: center;
+`
+
+export const Column = styled.div`
+width:30%;
+height:auto;
+/* border:1px solid blue; */
+display:flex;
+flex-direction: column;
+`
+
+export const SubTitleWrapper = styled.div`
+width:100%;
+height:50px;
+/* border: 1px red solid; */
+display:flex;
+justify-content: center;
+align-items: center;
+`
+
+export const SubTitle = styled.div`
+width:30%;
+height:50px;
+font-size: 23px;
+font-weight: 600;
+display: flex;
+align-items: center;
+justify-content: center;
+/* border:1px orange solid; */
+`
+
+export const BelowPart = styled.div`
+width:90%;
+height:60px;
+display:flex;
+align-items: center;
+justify-content: space-between;
+`
+
+export const Waiting = styled.div`
+width:auto;
+height:30px;
+display: flex;
+align-items: center;
+`
+
+export const NextButton = styled.button`
+width:100px;
+height:40px;
+border:none;
+border-radius: 10px;
+background-color: blueviolet;
+color:white;
+font-size:18px;
+font-weight: 500;
+cursor: pointer;
+`
